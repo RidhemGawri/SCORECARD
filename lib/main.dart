88ad0d02da4be99f40f104251feea19eebded4f9
RidhemGawri <ridhemgawri293@gmail.com>
2023-02-18 18:21:28 -0800

    Notification added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,43 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:resultproject/screens/auth/splash_screen.dart';
 
-
 void main() async {
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+          channelKey: 'basic_channel',
+          channelName: 'Basic notification',
+          channelDescription: 'Notification channel'),
+    ],
+    debug: true,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) => {
+       if(!isAllowed){
+         AwesomeNotifications().requestPermissionToSendNotifications(),
+       } 
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
